a1c6f5326e657fb794f50eaa942dd1c9eb3febba
Teitei011 <stefantleal14@gmail.com>
2021-06-07 19:41:09 -0300

    The beggining

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import Home from "./HomePage";
+import Error from "./ErrorPage";
+import About from "./AboutPage";
+import AuthWrapper from "./AuthWrapper";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+
+
+export {
+    Home,
+    Error,
+    About,
+    AuthWrapper,
+    Cart,
+    Checkout,
+    PrivateRoute,
+    Products,
+    SingleProduct
+}
