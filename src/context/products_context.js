94a04536596b1e4c1fbabbe89519439981f0fe60
Teitei011 <stefantleal14@gmail.com>
2021-06-14 10:41:45 -0300

    Single Product Images

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,8 +15,8 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
-  products_loading:false,
-  products_error:false,
+  products_loading: false,
+  products_error: false,
   products: [],
   featured_products: [],
   single_product_loading: false,
@@ -37,37 +37,39 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
 
-  const fetchProducts = async(url) =>{
-    dispatch({type:GET_PRODUCTS_BEGIN})
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
 
-      try{
-        const response = await axios.get(url)
-        const products = response.data
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
 
-        dispatch({ type: GET_PRODUCTS_SUCCESS, payload:products})
-      } catch(error){
-        dispatch({ type:GET_PRODUCTS_ERROR})
-      }
-  }
-
-
-    const fetchSingleProduct = async(url) =>{
-      dispatch({type: GET_SINGLE_PRODUCT_BEGIN});
-
-      try{
-        const response = await axios.get(url);
-        const singleProduct = response.data;
-        dispatch({type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct});
-      } catch(error){
-        dispatch({type:GET_SINGLE_PRODUCT_ERROR})
-      }
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
     }
+  };
 
-  useEffect(()=>{fetchProducts(url)}, [])
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
 
+    try {
+      const response = await axios.get(url);
+      const singleProduct = response.data;
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
 
   return (
-    <ProductsContext.Provider value={{...state, openSidebar, closeSidebar}}>
+    <ProductsContext.Provider
+      value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}
+    >
       {children}
     </ProductsContext.Provider>
   );
