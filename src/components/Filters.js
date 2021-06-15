1454238109cce977d4c22089f97388dc95301558
Teitei011 <stefantleal14@gmail.com>
2021-06-15 12:45:05 -0300

    My entire morning

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,7 +5,11 @@ import { getUniqueValues, formatPrice } from '../utils/helpers'
 import { FaCheck } from 'react-icons/fa'
 
 const Filters = () => {
-  return <h4>filters</h4>
+  return (
+    <Wrapper>
+      Filters
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.section`
